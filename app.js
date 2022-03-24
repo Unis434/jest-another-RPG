144d32c5a972883895313a2e5946d82722a73651
Unisa Kamara <unisk434@gmail.com>
2022-03-24 10:54:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,34 +5,3 @@ const inquirer = require('inquirer');
 const Enemy = require('./Enemy');
 const Player = require('./Player');
 
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-// requiring data
-const {Game} = require('./lib/Game');
-
-// sets an environment variable for port
-const PORT = process.env.PORT || 5500;
-
-// to instantiate the server
-const app = express();
-
-// parse incoming string or array data
-app.use(express.urlencoded({extended: true}));
-//parse incoming JSON data
-app.use(express.json());
-// instruct the server to make certain files readily available
-app.use(express.static('lib'));
-
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-// add routes
-// app.get('/',(req, res) => {
-//     res.json({message: 'Hello World!'})
-// });
-
-// to make our server listen
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`)
-});
