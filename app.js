09c78458800fc36f4163fbcfd3141dffff61b52c
Unisa Kamara <unisk434@gmail.com>
2022-03-25 10:09:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const Game = require('./lib/Game');
+const http = require('http');
+const port = process.env.PORT || 3000
 
-new Game().initializeGame();
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
