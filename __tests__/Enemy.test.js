bba26b8a1db0b45157614c348cc72275b637bd74
Unisa Kamara <unisk434@gmail.com>
2022-03-27 02:07:11 +0000

    Final commit due to refactoring

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -1,62 +1,60 @@
-const Enemy = require('../lib/Enemy.js');
-const Potion = require('../lib/Potion.js');
+const { test, expect } = require('@jest/globals');
+const Enemy = require('../lib/Enemy');
+const Potion = require('../lib/Potion');
 
-jest.mock('../lib/Potion.js');
+jest.mock('../lib/Potion');
 
-test('creates an enemy object', () => {
-  const enemy = new Enemy('Saffa', 'sword');
+test('Checks for proper enemy obj creation', () => {
+    const enemy = new Enemy('goblin', 'sword');
 
-  expect(enemy.name).toBe('Saffa');
-  expect(enemy.weapon).toBe('sword');
-  expect(enemy.health).toEqual(expect.any(Number));
-  expect(enemy.strength).toEqual(expect.any(Number));
-  expect(enemy.agility).toEqual(expect.any(Number));
-  expect(enemy.potion).toEqual(expect.any(Object));
-});
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+})
 
 test("gets enemy's health value", () => {
-    const enemy = new Enemy('Saffa', 'sword');
-  
-    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
-  });
-  
-  test('checks if enemy is alive or not', () => {
-    const enemy = new Enemy('Saffa', 'sword');
-  
-    expect(enemy.isAlive()).toBeTruthy();
-  
-    enemy.health = 0;
-  
-    expect(enemy.isAlive()).toBeFalsy();
-  });
-  
+    const enemy = new Enemy('goblin', 'sword');
 
-  
-  test("gets enemy's attack value", () => {
-    const enemy = new Enemy('Saffa', 'sword');
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
     enemy.strength = 10;
-  
+
     expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
     expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
-  });
-  
-  test("subtracts from enemy's health", () => {
-    const enemy = new Enemy('Saffa', 'sword');
+});
+
+test("subtracts from enemy's health", () => {
+    const enemy = new Enemy('goblin', 'sword');
     const oldHealth = enemy.health;
-  
+
     enemy.reduceHealth(5);
-  
+
     expect(enemy.health).toBe(oldHealth - 5);
-  
+
     enemy.reduceHealth(99999);
-  
+
     expect(enemy.health).toBe(0);
-  });
+});
 
-  test('gets a description of the enemy', () => {
-    const enemy = new Enemy('Saffa', 'sword');
-  
-    expect(enemy.getDescription()).toEqual(expect.stringContaining('Saffa'));
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
     expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
-  });
-
+})
--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,19 +1,16 @@
-const Potion = require('../lib/potion');
-
-console.log(new Potion());
+const Potion = require('../lib/Potion');
 
 test('creates a health potion object', () => {
-  const potion = new Potion('health');
+    const potion = new Potion('health');
 
-  expect(potion.name).toBe('health');
-  expect(potion.value).toEqual(expect.any(Number));
-});
-
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+})
 
 test('creates a random potion object', () => {
-  const potion = new Potion();
+    const potion = new Potion();
 
-  expect(potion.name).toEqual(expect.any(String));
-  expect(potion.name.length).toBeGreaterThan(0);
-  expect(potion.value).toEqual(expect.any(Number));
-});
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+})
